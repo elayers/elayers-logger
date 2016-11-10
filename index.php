1aebdd00bb201af3dd6b2db210ac480954c07846
Shakti Kathpalia <Shakti Kathpalia>
2016-11-10 14:31:40 +0530

    updates

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With"); 
 $logs = $_POST['console_logs'];
+if(sizeof($logs){
 foreach($logs as $log){
     error_log("CONSOLE_LOG: ".$log);
+}
 }
