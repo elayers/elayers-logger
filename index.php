5e599074d9fec532def43583cc2af62d17b0d102
Shakti Kathpalia <Shakti Kathpalia>
2016-11-10 14:26:35 +0530

    updates

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php $logs = $_POST['console_logs'];
+<?php header('Access-Control-Allow-Origin: *');  
+$logs = $_POST['console_logs'];
 foreach($logs as $log){
     error_log("CONSOLE_LOG: ".$log);
 }
